97c9e75e15b351f438711476f3b80c4be27cbe18
mservillon <mackservillon@gmail.com>
2023-10-08 11:28:05 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import config from './aws-exports'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
